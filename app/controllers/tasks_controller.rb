25415b1954b7685308aa25f463270ff1092febda
Fuka0102 <https://github.com/Fuka0102/todo.git>
2018-09-27 19:58:26 +0900

    show?

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,7 +18,28 @@ class TasksController < ApplicationController
   end
 
   def show
-     @task=Task.find_by(id:params[:id])
+     @task=Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+
+    @task.destroy
+    redirect_to tasks_path
   end
 
     private
