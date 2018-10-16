933dd646cdcbea93e9c9387b5a5e800715f48f6d
Fuka0102 <https://github.com/Fuka0102/todo.git>
2018-10-16 23:34:16 +0900

    login?

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/index'
   root to: 'tasks#index' 
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
   
+
 end
