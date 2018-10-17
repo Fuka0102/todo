f74f02a54a8d5306c4861a057408874ea079016d
Fuka0102 <https://github.com/Fuka0102/todo.git>
2018-10-17 23:04:09 +0900

    migration?2

--- db/migrate/20181017134626_add_password_to_users.rb
+++ db/migrate/20181017134626_add_password_to_users.rb
@@ -1,5 +1,5 @@
 class AddPasswordToUsers < ActiveRecord::Migration[5.2]
   def change
-      change_column :users, :password, :string　
+      add_column :users, :password, :string　
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_125425) do
+ActiveRecord::Schema.define(version: 2018_10_17_134626) do
 
   create_table "ta", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,11 +25,7 @@ ActiveRecord::Schema.define(version: 2018_10_17_125425) do
     t.date "deadline"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'string　' for column 'password'
 
 end
